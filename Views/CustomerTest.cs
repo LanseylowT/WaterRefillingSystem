889132b5f9f4df1d542ee394724efd7e9de39147
LanseylowT <donlancelotknight123@gmail.com>
2024-09-26 16:44:53 +0800

    My another shit commit

---
+++ Views/CustomerTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WaterRefillingSystem.Views
+{
+    public partial class CustomerTest : Form
+    {
+        public CustomerTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
