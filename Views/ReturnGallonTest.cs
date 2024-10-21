36331bb0625e63ec9740d69bf7e7dc159f7cfa8c
LanseylowT <donlancelotknight123@gmail.com>
2024-10-22 03:31:43 +0800

    can add values into the return gallon

--- Views/ReturnGallonTest.cs
+++ Views/ReturnGallonTest.cs
@@ -1,12 +1,51 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WaterRefillingSystem.Data;
+using WaterRefillingSystem.Models;
+using WaterRefillingSystem.Repository;
 
 namespace WaterRefillingSystem.Views
 {
     public partial class ReturnGallonTest : Form
     {
-        public ReturnGallonTest()
+        private Customer Customer { get; set; }
+        private Order Order { get; set; }
+        private readonly CustomerRepository _customerRepository;
+        public ReturnGallonTest(Order order)
         {
             InitializeComponent();
+            _customerRepository = new CustomerRepository(Commons.ConnectionString);
+            Order = order;
+        }
+
+        private void LoadData()
+        {
+            cmbBorrowedContainer.Items.Clear();
+            if (Customer != null)
+            {
+                txtItemType.Text = Order.ItemId == 1 ? "Slim Container" : "Round Container";
+                for (int i = 1; i <= Order.BorrowedGallons; i++)
+                {
+                    cmbBorrowedContainer.Items.Add(i);
+                }
+
+                // Optionally, set the first item as the selected value
+                if (cmbBorrowedContainer.Items.Count > 0)
+                {
+                    cmbBorrowedContainer.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private async void ReturnGallonTest_Load(object sender, System.EventArgs e)
+        {
+            Customer = await _customerRepository.GetCustomerByIdAsyncSP(Order.CustomerId);
+        }
+
+        private void btnReturn_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
